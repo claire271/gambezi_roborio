1d6a557a4e15cf110a5a4e90b65d49d987b3d2be
tigerh <tigerrun1998@gmail.com>
2017-07-27 00:41:55 -0400

    Added lwsws launching and relaunching abilities.

--- gambezi_launcher.sh
+++ gambezi_launcher.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
 
+# Utility functions
+logfile="/home/admin/gambezi/gambezi.log"
+function start_lwsws {
+        if [ -f $logfile ]; then
+                rm $logfile
+        fi
+        lwsws > $logfile 2>&1 &
+}
+function kill_lwsws {
+        pids=$(ps -e | grep "lwsws$" | awk '{print $1}')
+        for pid in $pids; do
+                kill $pid
+        done
+}
+
+# Cleanup here
 function cleanup {
+        kill_lwsws
         exit
 }
 trap "cleanup" SIGTERM
 
-cd /home/admin/gambezi/
-touch test.txt
+# lwsws and other programs
+start_lwsws
 
+# Monitoring
 while true; do
-	echo "." >> test.txt
-	sleep 1
+        # Server didn't launch properly
+        if grep -q "init server failed" $logfile; then
+                kill_lwsws
+                start_lwsws
+        fi
+        # Some error occurred within the gambezi plugin
+        if grep -q "SHUTDOWN REQUESTED" $logfile; then
+                kill_lwsws
+                start_lwsws
+        fi
+
+        # Don't waste too many cpu cycles
+        sleep 5
 done
